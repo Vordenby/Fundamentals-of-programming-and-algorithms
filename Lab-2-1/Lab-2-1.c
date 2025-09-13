2d5a7ab7a9438cee96a9da584ed797264b748a3e
VORDENBY <ceo65-1@outlook.com>
2025-09-14 00:36:55 +0300

    1.1.5
    Resuming my lab-2 files

--- Lab-2-1/Lab-2-1.c
+++ Lab-2-1/Lab-2-1.c
@@ -1,6 +1,8 @@
 ﻿#include <stdio.h>
 
 int main() {
-	printf("	\n1\n2\n3\n"); // подзадание 1
-	printf("...", 1, 2, 3, 4);
+	printf("\n\t1\n\t\t2\n\t\t\t3\n"); // subtask 1-3
+	printf("%1d, %2d, %3d, %4d \n", 1,2,3,4); // subtask 4
+	printf("%10.5f\n", 12.234567); //subtask 5-6
+	printf("Остаток от деления %d на %d равен %d \n", 5, 2, 5%2); // subtask 7
 }
