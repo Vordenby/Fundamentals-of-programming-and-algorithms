4c4e5ff6ac106f2dd0131a67c65c17becb8318bf
VORDENBY <ceo65-1@outlook.com>
2025-09-14 13:29:56 +0300

    Got problems with modifiyng second task: "segmentation fault".

--- Lab-3-2/Lab-3-2.c
+++ Lab-3-2/Lab-3-2.c
@@ -1,15 +1,45 @@
 #include <stdio.h>
-#define D 2.54
+#define Eng 2.54
+#define Spain 2.32166
+#define StarLit 2.7076
+
+int choosing_number() {
+    int K;
+    printf("Какой дюйм вы выбираете?\n (1 - английский) \n (2 - Испанский) \n (3 - Старолитовский) ");
+    scanf("%d", K);
+    return K;
+    }
 
 void main() {
-    int dym; // eng inch = 2,54cm
+    
+    int dym, choose; // eng inch = 2,54cm
     float result;
 
     printf("Введите размер (дюймы)\n");
-
     scanf("%d", &dym);
 
-    result = D * dym;
+    choose = 0;
+
+    while (choose != 1 || choose != 2 || choose !=3) 
+    {
+        choose = choosing_number();
+
+        if (choose == 1) 
+        {
+            result = Eng * dym;
+        }
+        else if (choose == 2)
+        {
+            result = Spain * dym;
+        }
+        else if (choose == 3)
+        {
+            result = StarLit * dym;
+        }
+    };
+    
+    
+    
 
     printf("%d дюймов - это %.1f см\n", dym, result);
 
