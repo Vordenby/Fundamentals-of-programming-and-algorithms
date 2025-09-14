a28ab4d3acac9ed9cafa4c4348f7e934cced0e5a
VORDENBY <ceo65-1@outlook.com>
2025-09-14 12:32:47 +0300

    Added homework

---
+++ Lab-0/HomeWork/Homework.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <locale.h>
+
+void main() {
+    setlocale(LC_CTYPE, "");
+    
+    puts("  *\n********\n*      *\n*************************\n*                      *\n***********************\n");
+}
