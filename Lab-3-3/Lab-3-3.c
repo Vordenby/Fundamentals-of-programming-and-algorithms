202395d9d42207fe44309acf29766282f7c550c6
VORDENBY <ceo65-1@outlook.com>
2025-09-14 16:55:40 +0300

    Task 3 is done!

--- Lab-3-3/Lab-3-3.c
+++ Lab-3-3/Lab-3-3.c
@@ -18,16 +18,13 @@ int main()
     if (a/10 > 0 && b/10 > 0) {FLength = a/10 + b/10 + 1;}
     else if ((a/10 > 0 && b/10 == 0)||(b/10 > 0 && a/10 == 0)) {FLength = a/10 + 2;}
     else {FLength = 3;}
-    
-
-
-    printf("%d\n", FLength);
 
     printf("|");putchars('-', (FLength*3)+2);printf("|\n");
-    printf("|a*b|a+b|a-b|\n");
+    printf("|%*s|%*s|%*s|\n", FLength, "a*b", FLength, "a+b", FLength, "a-b");
     printf("|");putchars('-', (FLength*3)+2);printf("|\n");
-    printf("|%d*%d|", a, b); printf("%d+%d", a, b); printf("|%d-%d|\n", a, b);
+    printf("|%*d|%*d|%*d|\n", FLength, a*b, FLength, a+b, FLength, a-b);
     printf("|");putchars('-', (FLength*3)+2);printf("|\n");
-    printf("|%d", a*b);putchars(' ',(a*b)/10);printf("|%d", a+b);putchars(' ', (a+b)/10+1);printf("|%d", a-b);putchars(' ',(a-b)/10+1);printf("|\n");
+   
+    return 0;
 
 }
