d0d936d79c2e6a3a95ec8f11a6b159c53796df23
VORDENBY <ceo65-1@outlook.com>
2025-09-15 15:41:39 +0300

    Done

--- Lab-5-2/Lab-5-2.c
+++ Lab-5-2/Lab-5-2.c
@@ -1,13 +1,41 @@
 #include <stdio.h>
 #include <math.h>
 
-int Y_creator(int a, int b, int c) {
-    
+#define _USE_MATH_DEFINES
+
+int Y_creator(float b, float x) {
+    float res = exp(x) + pow(5.8, -b);
+
+    return res;
+
+}
+
+int B_creator(float a, float c) {
+    float res = pow(a, 2)+ pow(c, 1./2);
+
+    return res;
+
+}
+
+int A_Creator(float c, float x) {
+    float res = pow(c, 3)+log10(fabs(x));
+
+    return res;
+
 }
 
 int main() {
     
-    
+    float c = 1.3;
+    float x, a, b, y;
+
+    scanf("%f", &x);
+
+    a = A_Creator(c, x);
+    b = B_creator(a, c);
+    y = Y_creator(b, x);
+
+    printf("|c) %f |b) %f |a) %f |y) %f |\n", c, b, a, y);
 
     return 0;
 }
