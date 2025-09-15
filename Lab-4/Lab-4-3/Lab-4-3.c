2ef36f50959307291cf093c0dddc54ff41386bb9
VORDENBY <ceo65-1@outlook.com>
2025-09-15 14:11:58 +0300

    Task 3 done

--- Lab-4/Lab-4-3/Lab-4-3.c
+++ Lab-4/Lab-4-3/Lab-4-3.c
@@ -5,7 +5,6 @@ int main() {
     int N;
     scanf("%d", &N);
 
-    printf("a) %d\nb) %d\nc) %d\n", N%10, N/100, N/100 + N%10 + (N%100 - N%10));
-
+    printf("a) %d\nb) %d\nc) %d\n", N%10, N/100, N/100 + N/10 - N/100*10 + N%10);
     return 0;
 }
