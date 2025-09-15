40009d2d4216db254ef6c5371d21f1c3b94e3315
VORDENBY <ceo65-1@outlook.com>
2025-09-15 15:18:54 +0300

    Done

--- Lab-5-1/Lab-5-1.c
+++ Lab-5-1/Lab-5-1.c
@@ -14,14 +14,5 @@ int main() {
 
     double result = sin(gr);
 
-    printf("%e", result);
+    printf("%e\n", result);
 }
-
-/* *  Executing task in folder C: gcc -Wall -Wextra -g3 /home/vorden/Documents/GitHub/Fundamentals-of-programming-and-algorithms/Lab-5-1/Lab-5-1.c -o /home/vorden/Documents/GitHub/Fundamentals-of-programming-and-algorithms/Lab-5-1/output/Lab-5-1 
-
-/usr/lib/gcc/x86_64-unknown-linux-gnu/14.3.0/../../../../x86_64-unknown-linux-gnu/bin/ld: /tmp/ccaKLJie.o: in function `main':
-/home/vorden/Documents/GitHub/Fundamentals-of-programming-and-algorithms/Lab-5-1/Lab-5-1.c:15:(.text+0x86): undefined reference to `sin'
-collect2: error: ld returned 1 exit status
-
- *  The terminal process "gcc '-Wall', '-Wextra', '-g3', '/home/vorden/Documents/GitHub/Fundamentals-of-programming-and-algorithms/Lab-5-1/Lab-5-1.c', '-o', '/home/vorden/Documents/GitHub/Fundamentals-of-programming-and-algorithms/Lab-5-1/output/Lab-5-1'" failed to launch (exit code: 1). 
- *  Terminal will be reused by tasks, press any key to close it. */
