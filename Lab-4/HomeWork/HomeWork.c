9a2afb41313279db903e8711cf2329ecabc9a51f
VORDENBY <ceo65-1@outlook.com>
2025-09-15 14:28:17 +0300

    Added HomeWork

---
+++ Lab-4/HomeWork/HomeWork.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <locale.h>
+
+typedef int bool;
+#define true 1
+#define false 0
+
+int Is_Active(int a, int b, int c, int d) {
+    if (a+b+c+d >= 3) { return true; }
+    else { return false; }
+}
+
+void Check_Active(int a){
+    if (a == 1){ printf("\n\n*** Запись сигнализации включена! ***\n\n"); }
+    else { printf("\n\n Не о чем беспокоится! \n\n"); }
+}
+
+
+int main()
+
+{
+    bool A, B, C, D;
+    printf("Поэтапно вводите значения (0 - False, 1 - True) датчиков A B C D\n");
+    scanf("%d", &A);
+    scanf("%d", &B);
+    scanf("%d", &C);
+    scanf("%d", &D);
+
+    Check_Active(Is_Active(A, B, C, D));
+
+    return 0;
+}
