53c360ac90e70e8e1815b7fdc6567aae94fe6ee9
VORDENBY <ceo65-1@outlook.com>
2025-10-02 21:57:17 +0300

    redone third task

--- Lab-5-3/Lab-5-3.c
+++ Lab-5-3/Lab-5-3.c
@@ -1,63 +1,34 @@
 #include <stdio.h>
 #include <math.h>
-#include <stdbool.h>
-
-#define _USE_MATH_DEFINES
-
-int Y_creator(float b, float x) {
-    b = b * (-1);
-    float res = exp(x) + pow(5.8, b);
-
-    return res;
-
-}
-
-int B_creator(float a, float c) {
-    float res = pow(a, 2)+ pow(c, 0.5);
-
-    return res;
-
-}
-
-int A_Creator(float c, float x) {
-    float res = pow(c, 3)+log10(fabs(x));
-
-    return res;
-
-}
-
-bool Uslovie_A(int A, int B) {
-    
-    if ( ( A % 2 == 0 && B % 2 != 0 ) || ( B % 2 == 0 && A % 2 != 0 ) ) {
-        return true;
-    }
-    return 0;
-}
-
-bool Uslovie_B(int A, int B, int C) {
-
-    if ( A % 3 == 0 && B % 3 == 0 && C % 3 == 0 ) { 
-        return true; 
-    }
-    return 0;
-}
+#include <locale.h>
 
 int main() {
+    setlocale(LC_ALL, "Rus");
     
-    float c = 1.3;
-    float x, a, b, y;
-    int Opt_A, Opt_B;
-
-    scanf("%f", &x);
-
-    a = A_Creator(c, x);
-    b = B_creator(a, c);
-    y = Y_creator(b, x);
-
-    Opt_A = Uslovie_A((int)a, (int)b);
-    Opt_B = Uslovie_B((int)a, (int)b, (int)y);
-
-    printf("условие выполнено (1 - да, 0 - нет \na)\t%d\nв)\t%d\n)", Opt_A, Opt_B);
-
+    double a, b, y;
+    printf("Введите числа a, b, y: ");
+    scanf("%lf %lf %lf", &a, &b, &y);
+    
+    int A = (int)a;
+    int B = (int)b;
+    int C = (int)y;
+    
+    int a1 = (A % 2 == 0);
+    int a2 = (B % 2 == 0);
+    int cond_a = a1 * (1 - a2) + (1 - a1) * a2;
+    
+    int b1 = (A % 3 == 0);
+    int b2 = (B % 3 == 0);
+    int b3 = (C % 3 == 0);
+    int cond_b = b1 * b2 * b3;
+    
+    printf("Условие a: ");
+    printf("условие выполнено (%d - да, 0 - нет)\n", cond_a);
+    printf("Значение: %d\n", cond_a);
+    
+    printf("Условие b: ");
+    printf("условие выполнено (%d - да, 0 - нет)\n", cond_b);
+    printf("Значение: %d\n", cond_b);
+    
     return 0;
 }
