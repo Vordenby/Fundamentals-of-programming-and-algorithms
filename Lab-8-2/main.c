24ed4ba9b9057930b3b0c0eecddd3792ce41abd2
Vordenby <ceo65-1@outlook.com>
2025-10-03 12:43:08 +0300

    1.2.3

--- Lab-8-2/main.c
+++ Lab-8-2/main.c
@@ -3,15 +3,20 @@
 #include <math.h>
 
 int main(void) {
-    double x, y, w;
-    printf("y = |e^w - 2| - x^2\nВведите через пробел значения w и x: "); 
-    scanf("%lf %lf", &w, &x);
-
-    y = fabs((exp(w) - 2)) - pow(x, 2);
+    double x1, x2, y, w;
+    printf("y = |e^w - 2| - x^2\nВведите через пробел значения w и x1 x2: "); 
+    scanf("%lf %lf %lf", &w, &x1, &x2);
 
     int tab;
 
     printf("Шаг табуляции: ");scanf("%d", &tab);
 
-    
+    printf("\n______________\n");
+    printf("|  x  | f(x) |\n");
+    printf("______________\n");
+
+    for (double x = x1; x <= x2; x += tab) {
+        y = fabs(exp(w) - 2) - pow(x, 2);
+        printf("| %.2lf | %.4lf |\n", x, y);
+    }
 }
