619782b8426369d968e3f71d9ddc4927c97d1664
vordenby <ceo65v@gmail.com>
2025-10-13 12:50:34 +0300

    1

---
+++ Lab-9/Lab-9-2/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(void)
+{
+
+    int n;
+
+    scanf("%d", &n);
+
+    int first, second, third, fourth, fifth, sixth, seventh, eighth, nineth, tenth, eleventh, twelveth;
+
+    first = 0;
+
+    }
