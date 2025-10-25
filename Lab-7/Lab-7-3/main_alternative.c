6532b8703d7bcbb1cf79ad92dd382ff2b59bca47
VORDENBY <ceo65-1@outlook.com>
2025-10-26 01:39:05 +0300

    Added Scheme

--- Lab-7/Lab-7-3/main_alternative.c
+++ Lab-7/Lab-7-3/main_alternative.c
@@ -5,33 +5,38 @@
 int main(void){
     setlocale(LC_CTYPE, ".UTF-8");
     
-    int n;
-    scanf("%d", &n);
+    int N;
+    scanf("%d", &N);
     
-    if (n >= 1 && n <= 20) {
-        if (n % 10 == 1 && n != 11) {
-            printf("%d рубль", n);
-        }
-        else if ((n % 10 >= 2 && n % 10 <= 4) && !(n >= 12 && n <= 14)) {
-            printf("%d рубля", n);
-        }
-        else {
-            printf("%d рублей", n);
-        }
-    } else {
-        int last_digit = n % 10;
-        int last_two_digits = n % 100;
+    if (N >= 1 && N <= 20) 
+    {
+        if (N % 10 == 1 && N != 11) 
+        {
+            printf("%d рубль", N);
+            }
+        else if ((N % 10 >= 2 && N % 10 <= 4) && !(N >= 12 && N <= 14)) 
+        {
+            printf("%d рубля", N);
+            }
+        else 
+        {
+            printf("%d рублей", N);
+            }
+    } else 
+    {
+        int last_digit = N % 10;
+        int last_two_digits = N % 100;
         
         if (last_digit == 1 && last_two_digits != 11) {
-            printf("%d рубль", n);
-        }
+            printf("%d рубль", N);
+            }
         else if (last_digit >= 2 && last_digit <= 4 && !(last_two_digits >= 12 && last_two_digits <= 14)) {
-            printf("%d рубля", n);
-        }
+            printf("%d рубля", N);
+            }
         else {
-            printf("%d рублей", n);
+            printf("%d рублей", N);
+            }
         }
-    }
     
     return 0;
 }
