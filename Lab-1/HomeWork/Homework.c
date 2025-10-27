40f0469a2f400224bc94d690c2ad4701df5c28c6
vordenby <ceo65v@gmail.com>
2025-10-27 12:09:05 +0300

    1.0

--- Lab-1/HomeWork/Homework.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <locale.h>
-
-void main() {
-    setlocale(LC_CTYPE, "");
-    
-    puts("  *\n********\n*      *\n*************************\n*                      *\n***********************\n");
-}
