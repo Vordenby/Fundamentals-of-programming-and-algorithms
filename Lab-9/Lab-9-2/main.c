7bdb9cd88e6f8aba51a1cd518fde58d84cdd403d
vordenby <ceo65v@gmail.com>
2025-10-27 13:09:43 +0300

    done

--- Lab-9/Lab-9-2/main.c
+++ Lab-9/Lab-9-2/main.c
@@ -2,19 +2,35 @@
 #include <locale.h>
 #include <math.h>
 
-int fst(double num)
+int fst(int num)
 {
-    
+    int sum = 0;
+    while (num > 0) {
+        int digit = num % 10;
+
+        for (int i = 1; i <= 9; i+=2) {
+            if ( digit == i) {
+                sum+=digit;
+                break;
+                }
+            }
+        num /= 10;
+        }
+    return sum;
     }
 
 int main(void)
 {
 
-    long int n;
+    int n;
 
     scanf("%d", &n);
 
-    int result = fst(fabs(n));
+    if (n < 0) { n*=-1; }
+
+    int result = fst(n);
+
+    printf("%d", result);
 
     return 0;
     }
