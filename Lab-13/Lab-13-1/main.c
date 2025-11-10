07877e38abc92fa196ae5fd9530aa813a24f0afc
vordenby <ceo65v@gmail.com>
2025-11-10 15:04:04 +0300

    0.5

---
+++ Lab-13/Lab-13-1/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void) {
+
+    char Name[20];
+    char Snew[40] = "Hello, ";
+
+    printf("What's your name?\n> ");
+    
+    scanf("%19s", Name);
+
+    if (strlen(Snew) + strlen(Name) < sizeof(Snew)) {
+        strcat(Snew, Name);
+    } else {
+        printf("Your name is too big\n");
+        return 1;
+    }
+    
+    printf("%s!\n", Snew);
+
+    // 1.2
+
+    for (char * ptr = Snew; *ptr; ptr++) {
+
+        *ptr = toupper(*ptr);
+
+    }
+
+    printf("%s!\n", Snew);
+
+    for (char * ptr = Snew; *ptr; ++ptr) {
+
+        printf("%s\n", ptr);
+
+    }
+
+    return 0;
+}
