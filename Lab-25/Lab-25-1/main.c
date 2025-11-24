4cb64759b20f66ab9a12ac21a8bccb25941f8b3a
vordenby <ceo65v@gmail.com>
2025-11-24 15:20:06 +0300

    done again

--- Lab-25/Lab-25-1/main.c
+++ Lab-25/Lab-25-1/main.c
@@ -4,9 +4,20 @@
 
 int main(void) {
 
+    setlocale(LC_ALL, ".UTF-8");
+
     int m = 1+rand()%10;
 
-    
+    printf("Угадайте число\n> ");
+
+    int k;
+    scanf("%d", &k);
+
+    if (k == m) {
+        printf("Вы угадали!\n");
+        } else {
+            printf("Вы не угадали :(\n");
+            }
 
     return 0;
 
