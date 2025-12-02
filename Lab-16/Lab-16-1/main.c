77bc8d4155dc81582a6f6989795a0688e2ea1d7c
Vordenby <ceo65-1@outlook.com>
2025-12-03 00:27:57 +0300

    16 2 almost done

---
+++ Lab-16/Lab-16-1/main.c
@@ -0,0 +1,57 @@
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void full_elements(double* ptr_array, int n) {
+    for (int i = 0; i < n; i++) {
+
+        *(ptr_array+i) = i* 200.0 / 45.0;
+
+    }
+}
+
+void calc_elements(double* ptr_array, int n) {
+
+    for (int i = 0; i < n; i++) {
+
+        *(ptr_array + i) = (double)((int)*(ptr_array+i));
+
+    }
+
+}
+
+void put_elements(double* ptr_array, int n) {
+
+    for (int i = 0; i < n; i++) {
+
+        printf("array[%d] = %.2f\n", i+1, *(ptr_array + i));
+
+    }
+
+}
+
+int main() {
+
+    double * ptr_array;
+
+    int size;
+
+    scanf("%d", &size);
+
+    ptr_array = (double *)malloc(size * sizeof(double));
+    if (ptr_array == NULL) {
+        puts("ERROR");
+        return -1;
+    }
+
+    full_elements(ptr_array, size);
+    put_elements(ptr_array, size);
+    puts("\n\n\n");
+    calc_elements(ptr_array, size);
+    put_elements(ptr_array, size);
+
+    free(ptr_array);
+
+    return 0;
+
+}
