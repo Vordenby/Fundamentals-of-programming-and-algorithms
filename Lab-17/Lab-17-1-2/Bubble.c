92115bb8efb58073e1992b2934d7450b2e370635
Vordenby <ceo65-1@outlook.com>
2025-12-06 17:19:12 +0300

    done

---
+++ Lab-17/Lab-17-1-2/Bubble.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <time.h>
+
+void BubbleSort(int* ptr_arr, int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - 1 - i; j++) {
+            int temp = ptr_arr[j];
+            if (ptr_arr[j] > ptr_arr[j+1]) {
+                ptr_arr[j] = ptr_arr[j+1];
+                ptr_arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+void PrintArray(int* ptr_arr, int n) {
+    for (int i = 0; i < n; i++) printf("%d\n", ptr_arr[i]);
+}
+
+void GenArray(int* ptr_array, int n) {
+    for (int i = 0; i < n; i++) *(ptr_array+i) = 1 + rand() % (1000 - 1);
+}
+
+void sort_kokteil(int* ptrarr, int n) {
+    int start = 0;
+    int end = n - 1;
+    int swapped;
+    do {
+        swapped = 0;
+        for (int i = start; i < end; i++) {
+            if (ptrarr[i] > ptrarr[i + 1]) {
+                int temp = ptrarr[i];
+                ptrarr[i] = ptrarr[i + 1];
+                ptrarr[i + 1] = temp;
+                swapped = 1;
+            }
+        }
+        if (!swapped) break;
+        swapped = 0;
+        for (int i = end; i > start; i--) {
+            if (ptrarr[i] < ptrarr[i - 1]) {
+                int temp = ptrarr[i];
+                ptrarr[i] = ptrarr[i - 1];
+                ptrarr[i - 1] = temp;
+                swapped = 1;
+            }
+        }
+        end--;
+    } while (swapped);
+}
+
+void sort_select(int* ptrarr, int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int min_idx = i;
+        for (int j = i + 1; j < n; j++) {
+            if (ptrarr[j] < ptrarr[min_idx]) {
+                min_idx = j;
+            }
+        }
+        int temp = ptrarr[i];
+        ptrarr[i] = ptrarr[min_idx];
+        ptrarr[min_idx] = temp;
+    }
+}
+
+void sort_insert(int* ptrarr, int n) {
+    for (int i = 1; i < n; i++) {
+        int key = ptrarr[i];
+        int j = i - 1;
+        while (j >= 0 && ptrarr[j] > key) {
+            ptrarr[j + 1] = ptrarr[j];
+            j--;
+        }
+        ptrarr[j + 1] = key;
+    }
+}
+
+int main() {
+
+    setlocale(LC_ALL, ".UTF-8");
+
+    int n;
+    scanf("%d", &n);
+
+    int *array = (int *)malloc(n * sizeof(int));
+    if (array == NULL) return -1;
+
+    GenArray(array, n);
+
+    clock_t start = clock();
+    sort_insert(array, n);
+    clock_t end = clock();
+    double time = (double)(end - start) / CLOCKS_PER_SEC;
+
+    printf("Время: %.10f \n", time);
+    // PrintArray(array, n);
+
+    free(array);
+    return 0;
+}
